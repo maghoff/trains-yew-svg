dc58b5146b1483a77024954dfe690d2c8aeed15b
Magnus Hoff <maghoff@gmail.com>
2020-02-29 21:23:33 +0100

    Render nice honeycomb

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,35 @@ impl Component for App {
             "Click me!"
         };
 
+        fn hex(q: i32, r: i32) -> Html {
+            let x = q * 45;
+            let y = q * 26 + r * 52;
+
+            html! {
+                <path
+                    class="hex"
+                    d={
+                        format!("M500,500 m-15,-26 m{},{} l30,0 l15,26 l-15,26 l-30,0 l-15,-26 z", x, y)
+                    }
+                />
+            }
+        }
+
+        use std::cmp::{max, min};
+
+        let coords = (-3..4).into_iter().flat_map(|q| {
+            (max(-3, -3 - q)..min(4, 4 - q))
+                .into_iter()
+                .map(move |r| (q, r))
+        });
+
         html! {
-            <button onclick=&self.onclick>{ button_text }</button>
+            <>
+                <svg viewBox="0 0 1000 1000" style="width: 1000px; height: 1000px">
+                    { coords.into_iter().map(|(q, r)| hex(q, r)).collect::<Html>() }
+                </svg>
+                <button onclick=&self.onclick>{ button_text }</button>
+            </>
         }
     }
 }
