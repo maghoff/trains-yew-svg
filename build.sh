85dc40dfcda32c6368f28a09439ba8e1dd050cc9
Magnus Hovland Hoff <maghoff@gmail.com>
2020-03-02 09:04:10 +0100

    Add dependency install instructions

--- build.sh
+++ build.sh
@@ -4,6 +4,18 @@ set -e
 
 export RUSTFLAGS=""
 
+if ! command -v wasm-pack > /dev/null
+then
+    echo "Install wasm-pack: https://rustwasm.github.io/wasm-pack/installer/" >&2
+    exit 1
+fi
+
+if ! command -v rollup > /dev/null
+then
+    echo "Install rollup: npm install --global rollup" >&2
+    exit 1
+fi
+
 # Got working with wasm-pack 0.9.1
 wasm-pack build --target web
 
